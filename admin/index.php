33c59ffd5755519a72f994b2b9a7f74a037c238f
truongddph34600 <truongddph34600@fpt.edu.vn>
2025-03-23 18:58:37 +0700

    fix index admmin

--- admin/index.php
+++ admin/index.php
@@ -5,6 +5,13 @@ if(!isset($_SESSION['admin'])){
   header('location:login.php');
 }
 else{
-
+include_once('../model/database.php');
+	include 'include/menu.php';
+	include 'include/header.php';
+	include 'controller.php';
+	/*echo '<div class="container-fluid">
+		aaaaaaa
+	</div>';*/
+	include 'include/footer.php';
 }
  ?>
