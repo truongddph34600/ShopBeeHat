88801b0a9a71924e34f786ebb18908e2cdc6540d
truongddph34600 <truongddph34600@fpt.edu.vn>
2025-03-23 19:00:56 +0700

    update controller admmin

--- admin/controller.php
+++ admin/controller.php
@@ -11,8 +11,30 @@
                         }else{
                             include('danhmuc/danhmuc.php');
                         }
+                        break;
+ case 'sanpham':
+                         if($check['Quyen']>2){
+                            echo('<center> BẠN KHÔNG CÓ QUYỀN TRUY CẬP!</center>');
+                        }else{
+                            include('sanpham/sanpham.php');
+                        }
 
                         break;
+                    case 'mau':
+                         if($check['Quyen']>2){
+                            echo('<center> BẠN KHÔNG CÓ QUYỀN TRUY CẬP!</center>');
+                        }else{
+                             include('mau/mau.php');
+                        }
 
+                        break;
+                    case 'xldathang':
+                         if($check['Quyen']==3 || $check['Quyen']==5 ){
+                            echo('<center> BẠN KHÔNG CÓ QUYỀN TRUY CẬP!</center>');
+                        }else{
+                             include('dondathang/main.php');
+                        }
+
+                        break;
 
     ?>
