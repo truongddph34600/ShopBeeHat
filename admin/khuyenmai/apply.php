23bf0d2898436b75eebf332d5ecbc1d4d48167e1
Hoany04 <ynguyenhoan@gmail.com>
2025-03-24 18:29:37 +0700

    chức năng và tiện ích

--- admin/khuyenmai/apply.php
+++ admin/khuyenmai/apply.php
@@ -156,3 +156,27 @@ if(isset($_POST['th'])){//nếu như bấm vài nút thương hiệu
 }
 
 ?>
+
+<script language="javascript">
+// Chức năng chọn hết
+document.getElementById("btn1").onclick = function() {
+    // Lấy danh sách checkbox
+    var checkboxes = document.getElementsByName('chon[]');
+
+    // Lặp và thiết lập checked
+    for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = true;
+    }
+};
+
+// Chức năng bỏ chọn hết
+document.getElementById("btn2").onclick = function() {
+    // Lấy danh sách checkbox
+    var checkboxes = document.getElementsByName('chon[]');
+
+    // Lặp và thiết lập Uncheck
+    for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = false;
+    }
+};
+</script>
