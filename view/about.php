7e8ead4e7c61628b0c697b8670f5d94726389f5b
truongddph34600 <truongddph34600@fpt.edu.vn>
2025-03-25 17:39:03 +0700

    update trang gioi thieu nguoi dung

---
+++ view/about.php
@@ -0,0 +1,19 @@
+<div class="breadcrumbs">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <p class="bread"><span><a href="index.html">Trang chủ</a></span> / <span>về chúng tôi</span></p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="colorlib-about">
+    <div class="container">
+        <div class="row row-pb-lg">
+            <div class="col-sm-6 mb-3">
+                <div class="video colorlib-video" style="background-image: url('webroot/image/logo/logos.jpg');">
+                    <a href="https://vimeo.com/channels/staffpicks/93951774" class="popup-vimeo"><i
+                            class="fas fa-play"></i></i></a>
+                    <div class="overlay"></div>
+                </div>
+            </div>
