41824db146a153a99564a3af55d919e129ccfae3
truongddph34600 <truongddph34600@fpt.edu.vn>
2025-03-25 17:39:40 +0700

    update trang about nguoi dung

--- view/about.php
+++ view/about.php
@@ -17,3 +17,22 @@
                     <div class="overlay"></div>
                 </div>
             </div>
+        <div class="col-sm-6">
+                        <div class="about-wrap">
+                            <h2>BeeHat - Cửa hàng mũ thời trang cao cấp</h2>
+                            <p>"Cửa hàng chuyên cung cấp các loại quần áo thời trang đa dạng và phong cách. Khách hàng có
+                                thể
+                                tìm thấy một bộ sưu tập đa dạng. Cửa hàng cam kết mang đến cho khách hàng những sản phẩm chất
+                                lượng, thời trang
+                                và phù hợp với xu hướng mới nhất. Bên cạnh đó, đội ngũ nhân viên chuyên nghiệp và nhiệt tình sẵn
+                                sàng tư vấn để giúp khách hàng lựa chọn những bộ đồ phù hợp với phong cách và nhu cầu của
+                                họ."</p>
+                            <p>"Nằm tại trung tâm thành phố, cửa hàng là địa điểm mua sắm lý tưởng cho những người yêu
+                                thích thời trang trẻ trung. Với không gian rộng rãi và trưng bày cẩn thận, khách hàng có thể dễ
+                                dàng lựa
+                                chọn và khám phá những mẫu quần áo mới nhất. Cửa hàng cung cấp các thương hiệu nổi tiếng và
+                                sản phẩm chất lượng, mang đến cho khách hàng sự tin tưởng và sự hài lòng. Đội ngũ nhân viên thân
+                                thiện và am hiểu về thời trang sẵn sàng đáp ứng mọi yêu cầu và mang đến trải nghiệm mua sắm thú vị
+                                và thoải mái."</p>
+                        </div>
+                    </div>
