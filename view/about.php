f214c39b4e6e819ed730dd7b802a5d28f297b401
truongddph34600 <truongddph34600@fpt.edu.vn>
2025-03-25 17:40:03 +0700

    update trang about nguoi dung 2

--- view/about.php
+++ view/about.php
@@ -36,3 +36,30 @@
                                 và thoải mái."</p>
                         </div>
                     </div>
+                </div>
+                    </div>
+                </div>
+                <div class="flexslider">
+                    <img src="webroot/image/slider/brand-2.jpg" alt="" width="100%" height="50%">
+                </div>
+                <div class="flexslider">
+                    <h2 class="text-center">SPONSORSHIP</h2>
+                    <p class="text-center">Ngắm nhìn những bức ảnh từ khách hàng của chúng tôi</p>
+                    <div class="row justify-content-center">
+                        <div class="col-6 col-sm-4 col-md-2 p-2">
+                            <img src="webroot/image/brand/spon1.jpeg" class="img-fluid" alt="Sponsor 1">
+                        </div>
+                        <div class="col-6 col-sm-4 col-md-2 p-2">
+                            <img src="webroot/image/brand/spon2.jpeg" class="img-fluid" alt="Sponsor 2">
+                        </div>
+                        <div class="col-6 col-sm-4 col-md-2 p-2">
+                            <img src="webroot/image/brand/spon3.jpeg" class="img-fluid" alt="Sponsor 3">
+                        </div>
+                        <div class="col-6 col-sm-4 col-md-2 p-2">
+                            <img src="webroot/image/brand/spon4.jpeg" class="img-fluid" alt="Sponsor 4">
+                        </div>
+                        <div class="col-6 col-sm-4 col-md-2 p-2">
+                            <img src="webroot/image/brand/spon5.jpeg" class="img-fluid" alt="Sponsor 5">
+                        </div>
+                    </div>
+                </div>
