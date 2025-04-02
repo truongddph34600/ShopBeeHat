1d6652c9657135ca0702ac6bf14dc203676380aa
truongddph34600 <truongddph34600@fpt.edu.vn>
2025-04-02 12:19:38 +0700

    trang footer người dùng

---
+++ view/element/footer.php
@@ -0,0 +1,106 @@
+<footer id="colorlib-footer" role="contentinfo">
+    <div class="container">
+        <div class="row row-pb-md">
+            <div class="col-5 footer-col colorlib-widget ">
+                <h2 class="text-white fs-2">Bee Hat</h2>
+                <p class="text-white fs-6 ">Bee Hat chuyên cung cấp các mặt hàng thời trang cao cấp</p>
+                <p class="text-white fs-6 text mb-0"><i class="fa-solid fa-location-dot"></i>
+                    Địa chỉ: Cầu Diễn - Từ Liêm - Thành phố Hà Nội.</p>
+                <p class="text-white fs-6 text mb-0"><i class="fa-solid fa-phone"></i>
+                    Hotline: (+84) 123 456 789</p>
+                <p class="text-white fs-6 text mb-0"><i class="fa-solid fa-envelope"></i>
+                    Email: beehat@gmail.com</p>
+
+            </div>
+            <div class="col-3 footer-col colorlib-widget">
+                <h4 class="text-white fs-5 d-inline-block">Sản phẩm</h4>
+                <hr class="border border-white border-2 d-inline-block" style="width: 150px; margin: 0;">
+                <p>
+                <ul class="colorlib-footer-links">
+                    <li><a href="#" class="text-white fs-7">Mũ lưỡi trai</a></li>
+                    <li><a href="#" class="text-white fs-7">Mũ bảo hiểm</a></li>
+                    <li><a href="#" class="text-white fs-7">Mũ quai vành</a></li>
+                    <li><a href="#" class="text-white fs-7">Mũ trẻ em</a></li>
+                </ul>
+                </p>
+            </div>
+
+            <div class="col-3 footer-col colorlib-widget">
+                <h4 class="text-white fs-5">Chính Sách</h4>
+                <hr class="border border-white border-2" style="width: 200px; margin: 0; ">
+                <p>
+                <ul class="colorlib-footer-links">
+                    <li><a href="#" class="text-white fs-7">Hướng dẫn mua hàng</a></li>
+                    <li><a href="#" class="text-white fs-7">Chính sách vận chuyển</a></li>
+                    <li><a href="#" class="text-white fs-7">Chính sách đổi trả</a></li>
+                    <li><a href="#" class="text-white fs-7">Chính sách thanh toán</a></li>
+
+                </ul>
+                </p>
+            </div>
+
+            <!-- <div class="col-2 footer-col colorlib-widget">
+                <h4 class="text-white fs-5">Liên hệ</h4>
+                <p>
+                <ul class="colorlib-footer-links ">
+                    <li class="fs-7"><i class="fa-brands fa-facebook text-white"></i> <a href="#" class="text-white">Facebook</a></li>
+                    <li class="fs-7"><i class="fa-brands fa-tiktok text-white"></i> <a href="#" class="text-white">Tik tok</a></li>
+                    <li class="fs-7"><i class="fa-brands fa-instagram text-white"></i> <a href="#" class="text-white">Intagram</a></li>
+                    <li class="fs-7"><i class="fa-solid fa-shop text-white"></i> <a href="#" class="text-white">Shope</a></li>
+                </ul>
+                </p>
+            </div> -->
+
+        </div>
+    </div>
+    <hr class="border border-white border-2 ">
+    <div class="copy">
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <p>
+                    <span class="text-white">
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script> All rights reserved | Design by WD49
+                    </span>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+<div class="gototop js-top">
+    <a href="#" class="js-gotop"><i class="fa-solid fa-up-long"></i></a>
+</div>
+
+<!-- jQuery -->
+<script src="./webroot/js/jquery.min.js"></script>
+<!-- popper -->
+<script src="./webroot/js/popper.min.js"></script>
+<!-- bootstrap 4.1 -->
+<script src="./webroot/js/bootstrap.min.js"></script>
+<!-- jQuery easing -->
+<script src="./webroot/js/jquery.easing.1.3.js"></script>
+<!-- Waypoints -->
+<script src="./webroot/js/jquery.waypoints.min.js"></script>
+<!-- Flexslider -->
+<script src="./webroot/js/jquery.flexslider-min.js"></script>
+<!-- Owl carousel -->
+<script src="./webroot/js/owl.carousel.min.js"></script>
+<!-- Magnific Popup -->
+<script src="./webroot/js/jquery.magnific-popup.min.js"></script>
+<script src="./webroot/js/magnific-popup-options.js"></script>
+<!-- Date Picker -->
+<script src="./webroot/js/bootstrap-datepicker.js"></script>
+<!-- Stellar Parallax -->
+<script src="./webroot/js/jquery.stellar.min.js"></script>
+<!-- Main -->
+<script src="./webroot/js/main.js"></script>
+<!-- my style -->
+<script src="./webroot/js/style.js"></script>
+
+</body>
+
+</html>
