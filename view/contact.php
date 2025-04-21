ca9f0312e689388b8b6fb5f8cac9c5f570bee4f9
ducmanh2k4 <nguyenducmanh23122004@gmail.com>
2025-04-21 20:27:36 +0700

    add contact

---
+++ view/contact.php
@@ -0,0 +1,88 @@
+<div class="breadcrumbs">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <p class="bread"><span><a href="index.html">Trang Chủ</a></span> / <span>Liên Hệ</span></p>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="colorlib-contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h3>Thông tin liên lạc</h3>
+                <div class="row contact-info-wrap">
+                    <div class="col-md-3">
+                        <p><span><i class="fas fa-location-dot"></i></span> Cầu Diễn - Từ Liêm - Thành phố Hà Nội </p>
+                    </div>
+                    <div class="col-md-3">
+                        <p><span><i class="fas fa-phone"></i></span> <a href="tel://0931294567"> (+84) 123 456 789
+                            </a></p>
+                    </div>
+                    <div class="col-md-3">
+                        <p><span><i class="fas fa-paper-plane"></i></span> <a
+                                href="mailto:info@yoursite.com">Beehat@gmail.com</a></p>
+                    </div>
+                    <div class="col-md-3">
+                        <p><span><i class="fas fa-globe"></i></span> <a href="#">beehat.com</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="contact-wrap">
+                    <h3>Liên lạc</h3>
+                    <form action="#" class="contact-form">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="fname">Họ</label>
+                                    <input type="text" id="fname" class="form-control" placeholder="Họ">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="lname">Tên</label>
+                                    <input type="text" id="lname" class="form-control" placeholder="Tên">
+                                </div>
+                            </div>
+                            <div class="w-100"></div>
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="text" id="email" class="form-control" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="w-100"></div>
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <label for="subject">Tiêu đề</label>
+                                    <input type="text" id="subject" class="form-control" placeholder="Tiêu đề">
+                                </div>
+                            </div>
+                            <div class="w-100"></div>
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <label for="message">Nội dung</label>
+                                    <textarea name="message" id="message" cols="30" rows="10" class="form-control"
+                                        placeholder="Nội dung"></textarea>
+                                </div>
+                            </div>
+                            <div class="w-100"></div>
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <input type="submit" value="Gửi" class="btn btn-primary">
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div id="map" class="colorlib-map"></div>
+            </div>
+        </div>
+    </div>
+</div>
