faeac9a074409732812ac00a9c9bfb734e7de51a
truongddph34600 <truongddph34600@fpt.edu.vn>
2025-05-05 22:36:36 +0700

    commit thêm mục icon cho từng chức năng

--- admin/include/menu.php
+++ admin/include/menu.php
@@ -33,11 +33,9 @@
             <br>
             <!-- Sidebar - Brand -->
             <div class="text-center d-none d-md-inline">
-                <!-- <button class="rounded-circle border-0" id="sidebarToggle"></button> -->
                 <h4 class="text-center text-white fw-bold">Admin</h4>
             </div>
 
-            <!-- Divider -->
             <hr class=" sidebar-divider my-0">
 
             <!-- Nav Item - Dashboard -->
@@ -47,111 +45,78 @@
                     <span>Dashboard</span></a>
             </li>
 
-            <!-- Divider -->
             <hr class="sidebar-divider">
 
-            <!-- Heading -->
-            <!-- Nav Item - Charts -->
+            <!-- Orders Pending -->
             <?php
-        $sql="SELECT * FROM `hoadon` WHERE `TinhTrang` = 'chưa duyệt'";
+        $sql="SELECT * FROM hoadon WHERE TinhTrang = 'chưa duyệt'";
         $rs=mysqli_query($conn,$sql);
         $dem=mysqli_num_rows($rs);
       ?>
             <li class="nav-item">
                 <a class="nav-link" href="?action=xldathang">
-
+                    <i class="mdi mdi-cart menu-icon"></i>
                     <span>Đơn Đặt Hàng <sup style="border-radius: 10px;" class="badge-danger ">
                             &#160;<?php echo $dem ?>
                             &#160;</sup></span></a>
             </li>
-            <!-- Nav Item - Charts -->
+
+            <!-- Shipping -->
             <?php
-        $sql="SELECT * FROM `hoadon` WHERE NgayGiao is not null and TinhTrang='Đã duyệt' ORDER BY NgayGiao ASc";
-        $rs=mysqli_query($conn,$sql); 
+        $sql="SELECT * FROM hoadon WHERE NgayGiao is not null and TinhTrang='Đã duyệt' ORDER BY NgayGiao ASc";
+        $rs=mysqli_query($conn,$sql);
         $dem=mysqli_num_rows($rs);
       ?>
             <li class="nav-item">
                 <a class="nav-link" href="?action=giaohang">
-
+                    <i class="mdi mdi-truck-delivery menu-icon"></i>
                     <span>Giao Hàng <sup style="border-radius: 10px;" class="badge-danger ">
                             &#160;<?php echo $dem ?>
                             &#160;</sup></span></a>
-
-
             </li>
-            <!-- Nav Item - Pages Collapse Menu -->
+
+            <!-- Kho Hàng -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
-
+                    <i class="mdi mdi-eye menu-icon"></i>
                     <span>Kho Hàng</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <!-- <h6 class="collapse-header">Custom Components:</h6> -->
-                        <a class="collapse-item" href="?action=kho&view=xemdh">Xem đơn hàng</a>
-                        <a class="collapse-item" href="?action=kho&view=nhapkho">Xuất / Nhập kho</a>
-                        <a class="collapse-item" href="?action=kho&view=nhatky">Nhật ký Nhập Kho</a>
+                        <a class="collapse-item" href="?action=kho&view=xemdh"> Xem đơn hàng</a>
+                        <a class="collapse-item" href="?action=kho&view=nhapkho"> Xuất / Nhập kho</a>
+                        <a class="collapse-item" href="?action=kho&view=nhatky"> Nhật ký Nhập Kho</a>
                         <a class="collapse-item" href="?action=kho&view=nhatkyx">Nhật ký Xuất Kho</a>
                     </div>
                 </div>
             </li>
 
-            <!-- Nav Item - Utilities Collapse Menu -->
-            <!-- <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-          <i class="fas fa-fw fa-wrench"></i>
-          <span>Utilities</span>
-        </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Utilities:</h6>
-            <a class="collapse-item" href="utilities-color.html">Colors</a>
-            <a class="collapse-item" href="utilities-border.html">Borders</a>
-            <a class="collapse-item" href="utilities-animation.html">Animations</a>
-            <a class="collapse-item" href="utilities-other.html">Other</a>
-          </div>
-        </div>
-      </li> -->
-
-            <!-- Divider -->
-            <!-- <hr class="sidebar-divider"> -->
-
-            <!-- Heading -->
-            <!-- <div class="sidebar-heading">
-                Manage
-            </div> -->
-
-            <!-- Nav Item - Pages Collapse Menu -->
+            <!-- Danh Mục -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
                     aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-folder"></i>
+                    <i class="mdi mdi-format-list-bulleted menu-icon"></i>
                     <span>Danh Mục</span>
                 </a>
                 <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header"></h6>
                         <a class="collapse-item" href="?action=danhmuc&">Danh Sách</a>
                         <a class="collapse-item" href="?action=danhmuc&view=them">Thêm</a>
-                        <!-- <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-           <div class="collapse-divider"></div>
-           <h6 class="collapse-header">Other Pages:</h6>
-           <a class="collapse-item" href="404.html">404 Page</a>
-           <a class="collapse-item" href="blank.html">Blank Page</a> -->
                     </div>
                 </div>
             </li>
+
+            <!-- Sản Phẩm -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages1"
                     aria-expanded="true" aria-controls="collapsePages1">
-                    <i class="fas fa-fw fa-table"></i>
+                    <i class="mdi mdi-cube-outline menu-icon"></i>
                     <span>Sản Phẩm</span>
                 </a>
                 <div id="collapsePages1" class="collapse" aria-labelledby="headingPages"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header"></h6>
                         <a class="collapse-item" href="?action=sanpham">Danh Sách</a>
                         <a class="collapse-item" href="?action=sanpham&view=themsp">Thêm</a>
                         <a class="collapse-item" href="?action=mau&view=them">Thêm màu</a>
@@ -159,19 +124,24 @@
                 </div>
             </li>
 
+            <!-- Khuyến Mãi -->
             <li class="nav-item">
                 <a class="nav-link" href="?action=khuyenmai">
-                    <i class="fas fa-atom"></i>
+                    <i class="mdi mdi-sale menu-icon"></i>
                     <span>Khuyến Mãi</span></a>
             </li>
+
+            <!-- Doanh Thu -->
             <li class="nav-item">
                 <a class="nav-link" href="?action=danhthu">
-                    <i class="fas fa-atom"></i>
+                    <i class="mdi mdi-cash-multiple menu-icon"></i>
                     <span>Doanh Thu</span></a>
             </li>
+
+            <!-- Quản lý nhân viên -->
             <li class="nav-item">
                 <a class="nav-link" href="?action=nhanvien">
-                    <i class="fas fa-atom"></i>
+                    <i class="mdi mdi-account-group menu-icon"></i>
                     <span>Quản lý nhân viên</span></a>
             </li>
             <hr class="sidebar-divider d-none d-md-block">
