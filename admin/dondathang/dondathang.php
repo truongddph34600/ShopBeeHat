8acf6492bdd40c140280bc0704266bf9acd6e39d
ducmanh2k4 <nguyenducmanh23122004@gmail.com>
2025-05-17 20:10:52 +0700

    update giao dien don dat hang

--- admin/dondathang/dondathang.php
+++ admin/dondathang/dondathang.php
@@ -16,7 +16,7 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <?php 
+                    <?php
 			 		// lấy danh sách sản phẩm  theo phân trang.
 		            if(isset($_GET['trang'])){
 		                $trang=$_GET['trang'];
@@ -30,7 +30,7 @@
 			 		}else{
 			 			$sql="select * from hoadon Order  by NgayDat DESC LIMIT $from,30";
 			 		}
-			 		
+
 					$rs=mysqli_query($conn,$sql); $so=0;
 				 	while ($row=mysqli_fetch_array($rs)) { ?>
                     <tr>
@@ -90,13 +90,13 @@
 					 			$ds_spn1b="SELECT * from hoadon  where TinhTrang='".$dk2."'";
 					 		}else{
 					 			$ds_spn1b="select * from hoadon";
-					 		} 
+					 		}
 		            $query_dssp2=mysqli_query($conn,$ds_spn1b);
 		            $sosp=mysqli_num_rows($query_dssp2);
 		            $sotrang = ceil($sosp/30); ?>
         <hr>
         <ul class="pagination justify-content-center">
-            <?php for($x=1;$x<=$sotrang;$x++){ 
+            <?php for($x=1;$x<=$sotrang;$x++){
 						         	if($dk2===''){?>
             <li class="page-item">
                 <a class="page-link " href="index.php?action=xldathang&trang=<?php echo $x ?>"><?php echo $x ;?></a>
@@ -111,5 +111,6 @@
 
 
         </ul>
+
     </div>
 </div>
